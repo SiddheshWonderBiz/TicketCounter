5674abf0f9890fd2042395e94291f87a7d67606a
SiddheshWonderBiz <siddhesh.wayal@wonderbiz.in>
2025-07-30 16:37:13 +0530

    1 st commit

--- TicketCounter/Program.cs
+++ TicketCounter/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more inf
+namespace TicketCounter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hhhhhhhh");
+    }
+}
